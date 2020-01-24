7a273652c0258687b7538b3790764169860be5a8
Emediong Cosmas <emediongcosmas1@gmail.com>
2020-01-24 14:48:43 +0100

    Front end edit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>{{config('app.name', 'WedVendor')}}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('app-assets/css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/animate.css')}}">
+    
+    <link rel="stylesheet" href="{{asset('app-asssets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/magnific-popup.css')}}">
+
+    <link rel="stylesheet" href="{{asset('app-assets/css/aos.css')}}">
+
+    <link rel="stylesheet" href="{{asset('app-assets/css/ionicons.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('app-assets/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/jquery.timepicker.css')}}">
+
+    
+    <link rel="stylesheet" href="{{asset('app-assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/css/style.css')}}">
+  </head>
+  <body>
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html"><span>{{config('app.name', 'WedVendor')}}</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="destination.html" class="nav-link">Destination</a></li>
+	          <li class="nav-item"><a href="hotel-resto.html" class="nav-link">Hotels &amp; Restaurant</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item cta"><a href="#" class="nav-link">Book Now</a></li>
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <div class="hero-wrap js-fullheight" style="background-image: url({{asset('app-assets/images/bg_1.jpg')}});" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="col-md-7 ftco-animate mt-5" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Discover your nearest wedding vendor</h1>
+            <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">No struggles with online orders...</p>
+          </div>
+        </div>
+      </div>
+    </div>
